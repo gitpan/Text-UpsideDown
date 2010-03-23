3b4c19059a930b94a767997f853169a4cbfad408
Marcel Grünauer <marcel@cpan.org>
2010-03-23 16:54:17 +0000

    Import of MARCEL/Text-UpsideDown-1.100820 from CPAN.
    gitpan-cpan-distribution: Text-UpsideDown
    gitpan-cpan-version:      1.100820
    gitpan-cpan-path:         MARCEL/Text-UpsideDown-1.100820.tar.gz
    gitpan-cpan-author:       MARCEL
    gitpan-cpan-maturity:     released

--- lib/Text/UpsideDown.pm
+++ lib/Text/UpsideDown.pm
@@ -1,10 +1,12 @@
-package Text::UpsideDown;
 use 5.008;
-use warnings;
 use strict;
+use warnings;
+
+package Text::UpsideDown;
+our $VERSION = '1.100820';
+# ABSTRACT: Flip text upside-down using Unicode
 use charnames ':full';
 use Exporter qw(import);
-our $VERSION = '0.03';
 our @EXPORT  = ('upside_down');
 
 # mapping taken from
@@ -80,11 +82,18 @@ sub upside_down {
     join '' => reverse split '', $text;
 }
 1;
+
+
 __END__
+=pod
 
 =head1 NAME
 
 Text::UpsideDown - Flip text upside-down using Unicode
+
+=head1 VERSION
+
+version 1.100820
 
 =head1 SYNOPSIS
 
@@ -93,46 +102,50 @@ Text::UpsideDown - Flip text upside-down using Unicode
 
 =head1 DESCRIPTION
 
-This module exports only one function:
+This module exports only one function.
 
-=over 4
+=head1 FUNCTIONS
 
-=item C<upside_down>
+=head2 upside_down
 
 Takes a string and returns a string that looks like the characters have been
 turned upside-down and reversed so it could be read by turning your head
 upside-down. It uses special Unicode characters originally intended for
 mathematics and the like.
 
-=back
+=head1 INSTALLATION
+
+See perlmodinstall for information and options on installing Perl modules.
 
 =head1 BUGS AND LIMITATIONS
 
 No bugs have been reported.
 
 Please report any bugs or feature requests through the web interface at
-L<http://rt.cpan.org>.
-
-=head1 INSTALLATION
-
-See perlmodinstall for information and options on installing Perl modules.
+L<http://rt.cpan.org/Public/Dist/Display.html?Name=Text-UpsideDown>.
 
 =head1 AVAILABILITY
 
 The latest version of this module is available from the Comprehensive Perl
-Archive Network (CPAN). Visit <http://www.perl.com/CPAN/> to find a CPAN
-site near you. Or see L<http://search.cpan.org/dist/Text-UpsideDown/>.
+Archive Network (CPAN). Visit L<http://www.perl.com/CPAN/> to find a CPAN
+site near you, or see
+L<http://search.cpan.org/dist/Text-UpsideDown/>.
 
-=head1 AUTHORS
+The development version lives at
+L<http://github.com/hanekomu/Text-UpsideDown/>.
+Instead of sending patches, please fork this project using the standard git
+and github infrastructure.
 
-Marcel GrE<uuml>nauer, C<< <marcel@cpan.org> >>
+=head1 AUTHOR
+
+  Marcel Gruenauer <marcel@cpan.org>
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright 2007-2009 by the authors.
+This software is copyright (c) 2007 by Marcel Gruenauer.
 
-This library is free software; you can redistribute it and/or modify
-it under the same terms as Perl itself.
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
 
 =cut
 
